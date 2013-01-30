43c81774068b8e3f3d0400e971d2db39fcf77f00
vgeirnaert <gsfjohndoe@hotmail.com>
2013-01-30 19:59:29 +0100

    Further attempts to find a good ui design...

--- website/js/main.js
+++ website/js/main.js
@@ -1,1 +1,6 @@
-
+$(".checkbox").click(function(){
+    if($(this).hasClass('icon-check')){
+        $(this).removeClass('icon-check').addClass('icon-check-empty');}
+    else {
+        $(this).removeClass('icon-check-empty').addClass('icon-check');}
+});
