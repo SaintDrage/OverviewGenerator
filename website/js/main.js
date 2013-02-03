5716c914d156e603ba38c8cf091a16dac5c46386
vgeirnaert <gsfjohndoe@hotmail.com>
2013-02-03 01:36:01 +0100

    Settled on a design! Filters can be created on a separate page and will be listed in the Filters drop down menu. The main page will be used to set global settings, which includes setting which tabs contain which filters.
    Also added a color picker

--- website/js/main.js
+++ website/js/main.js
@@ -1,6 +1,12 @@
-$(".checkbox").click(function(){
-    if($(this).hasClass('icon-check')){
-        $(this).removeClass('icon-check').addClass('icon-check-empty');}
-    else {
-        $(this).removeClass('icon-check-empty').addClass('icon-check');}
+$('#newfilter').colorpicker().on('changeColor', function(ev) {
+	$('#newinput')[0].style.color = ev.color.toHex();
+});
+
+var myTitle = $('#newinput')[0].value;
+$('#newfilter').colorpicker().on('show', function(ev) {
+	myTitle = $('#newinput')[0].value;
+});
+
+$('#newfilter').colorpicker().on('hide', function(ev) {
+	$('#newinput')[0].value = myTitle;
 });
